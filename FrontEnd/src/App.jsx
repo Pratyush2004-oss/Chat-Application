60b92fd5f3f654da8914efca978d365edcbdd6cb
Pratyush2004-oss <pratyush@sitp.ac.in>
2024-09-18 20:17:31 +0530

    Chat

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 import {Routes,Route, Navigate} from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './Pages/Login/Login'
 import Signup from './Pages/Signup/Signup'
